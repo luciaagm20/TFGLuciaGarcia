57fa4621b350cf85fc83f971d75626453ba1728e
luciaagm20 <159811756+luciaagm20@users.noreply.github.com>
2024-06-04 18:22:35 +0200

    Autenticación FE

---
+++ frontend/src/components/LoginPage/LoginPage.jsx
@@ -0,0 +1,17 @@
+const Login = () => {
+    const { setToken } = useAuth();
+    const navigate = useNavigate();
+  
+    const handleLogin = () => {
+      setToken("this is a test token");
+      navigate("/", { replace: true });
+    };
+  
+    setTimeout(() => {
+      handleLogin();
+    }, 3 * 1000);
+  
+    return <>Login Page</>;
+  };
+  
+  export default Login;
